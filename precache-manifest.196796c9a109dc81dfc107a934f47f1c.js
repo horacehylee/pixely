27834439b015c2ce70933853b7fa782ce5309aba
Horace Lee <horacehylee2@gmail.com>
2019-07-03 22:24:01 +0800

    Updates

---
+++ precache-manifest.196796c9a109dc81dfc107a934f47f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d1e6d8a10520a8644765ffe9acdb2c",
+    "url": "/pixely/index.html"
+  },
+  {
+    "revision": "09cdb85ad5b7a1d325c9",
+    "url": "/pixely/static/css/main.31898227.chunk.css"
+  },
+  {
+    "revision": "ed7ebde7a8212be63468",
+    "url": "/pixely/static/js/2.f8e82fb3.chunk.js"
+  },
+  {
+    "revision": "09cdb85ad5b7a1d325c9",
+    "url": "/pixely/static/js/main.5ddd3ce6.chunk.js"
+  },
+  {
+    "revision": "3a7f809a9c601f66b52d",
+    "url": "/pixely/static/js/runtime~main.7d74c7c8.js"
+  }
+]);
