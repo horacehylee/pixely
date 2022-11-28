22a9fe1cc5c43084c53f4b1a7b93db80418dedfd
Michael Li <mingholi4-c@my.cityu.edu.hk>
2022-11-28 18:41:40 +0800

    Add feature - clickable dropdown menu

--- src/components/menu/FileMenu.tsx
+++ src/components/menu/FileMenu.tsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import DropDownMenu from "./DropdownMenu";
+
+const items = () => {
+  return ["Select", "Settings", "Save Image as", "Exit"];
+}
 
 const FileMenuItems: React.FC = () => {
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [selectItem, setSelectItem] = useState("");
+
+  const toggleDropDown = () => {
+    setShowDropDown(!showDropDown);
+  };
+
+  const dismissHandler = (event: React.FocusEvent<HTMLButtonElement>): void => {
+    if (event.currentTarget === event.target) {
+      setShowDropDown(false);
+    }
+  };
+
+  const itemSelection = (item: string): void => {
+    setSelectItem(item);
+  };
+  
   return (
-    <>File</>
+    <button
+      className={showDropDown ? "active" : "Menu-items Menu-button"}
+      onClick={(): void => toggleDropDown()}
+      onBlur={(e: React.FocusEvent<HTMLButtonElement>): void =>
+        dismissHandler(e)
+      }
+    > 
+      <label>File</label>
+      {showDropDown && (
+        <DropDownMenu
+          items={items()}
+          showDropDown={false}
+          toggleDropDown={(): void => toggleDropDown()}
+          itemSelection={itemSelection}
+        />
+      )}
+    </button>
   );
 };
 
